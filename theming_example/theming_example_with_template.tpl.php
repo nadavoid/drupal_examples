328d201e05a71502c38f3a7d7169b764162e0bbb
David Lanier <dlanier@allplayers.com>
2011-11-30 08:11:38 -0700

    Issue #1356556 adding theming_example

---
+++ theming_example/theming_example_with_template.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Template file for theming_example_with_template
+ *
+ * $content comes from arguments definition in hook_theme()
+ *
+ * <?php print '<!--' . print_r($content, TRUE) . '-->'; ?>
+ *
+ */
+?>
+<!-- theming_example_with_template template -->
+<div>
+<h3>This is in a template file.</h3>
+<?php  print $content; ?>
+</div>
+<!-- /theming_example_with_template template -->
